28e06e6ac7d69a530ab7295229f43d22c4de4071
SuxrobGM <suxrobGM@gmail.com>
2019-07-16 13:58:56 +0500

    changed cheque design

--- src/MedicalClinicQueue/Services/PrinterService.cs
+++ src/MedicalClinicQueue/Services/PrinterService.cs
@@ -1,14 +1,7 @@
 ﻿using MedicalClinicQueue.Data;
 using MedicalClinicQueue.Models;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Printing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MedicalClinicQueue.Services
 {
@@ -39,22 +32,33 @@ namespace MedicalClinicQueue.Services
                 var regular = new Font(FontFamily.GenericSansSerif, 10.0f, FontStyle.Regular);
                 var regularSmaller = new Font(FontFamily.GenericSansSerif, 8.0f, FontStyle.Regular);
                 var bold = new Font(FontFamily.GenericSansSerif, 10.0f, FontStyle.Bold);
-                var boldHigher = new Font(FontFamily.GenericSansSerif, 30.0f, FontStyle.Bold);
+                var boldHigher = new Font(FontFamily.GenericSansSerif, 50.0f, FontStyle.Bold);
+                var stringFormat = new StringFormat()
+                {
+                    LineAlignment = StringAlignment.Center,
+                    Alignment = StringAlignment.Center
+                };
 
-                graphics.DrawString(_company.Name, regular, Brushes.Black, 30, 10);
-                graphics.DrawLine(Pens.Black, 10, 30, 200, 30);
-                graphics.DrawString($"Время: {_serviceItem.LastTimestamp.ToShortTimeString()}", bold, Brushes.Black, 20, 50);
-                graphics.DrawString(_serviceItem.QueueCount.ToString(), boldHigher, Brushes.Black, 70, 80);
+                // Company name
+                graphics.DrawString(_company.Name, regular, Brushes.Black, 20, 10);
+                graphics.DrawLine(Pens.Black, 10, 30, 210, 30);
 
-                //Doctor name section
-                graphics.DrawRectangle(Pens.Black, 10, 150, 200, 40);
-                graphics.DrawString(_serviceItem.Name, bold, Brushes.Black, 20, 160);
+                // Number and time
+                graphics.DrawString($"Время: {_serviceItem.LastTimestamp.ToString("dd.MM.yyyy hh:mm")}", bold, Brushes.Black, 20, 50);
+                graphics.DrawString(_serviceItem.QueueCount.ToString(), boldHigher, Brushes.Black, 55, 65);
 
-                //Contacts section        
-                graphics.DrawString($"Контакты:", regular, Brushes.Black, 10, 210);
-                graphics.DrawRectangle(Pens.Black, 10, 225, 200, 120);
-                var sf = graphics.MeasureString(_company.Contacts, regularSmaller, 200);
-                graphics.DrawString(_company.Contacts, regularSmaller, Brushes.Black, new RectangleF(new Point(13, 230), sf), StringFormat.GenericTypographic);
+                // Doctor name section
+                var doctorNameSF = graphics.MeasureString(_serviceItem.Name, bold, 200, stringFormat);
+                graphics.DrawRectangle(Pens.Black, 10, 150, 200, 70);
+                //graphics.DrawString(_serviceItem.Name, bold, Brushes.Black, 20, 162);
+                graphics.DrawString(_serviceItem.Name, bold, Brushes.Black, new RectangleF(new PointF(20, 160), doctorNameSF), stringFormat);
+
+                // Contacts section        
+                //graphics.DrawString($"Контакты:", regular, Brushes.Black, 10, 210);
+                //graphics.DrawRectangle(Pens.Black, 10, 225, 200, 120);
+                var contactsSF = graphics.MeasureString(_company.Contacts, regularSmaller, 200);
+                graphics.DrawString(_company.Contacts, regularSmaller, Brushes.Black, new RectangleF(new Point(13, 250), contactsSF), StringFormat.GenericTypographic);
+                graphics.DrawLine(Pens.Black, 10, 300, 210, 300);
             }
         }        
     }
